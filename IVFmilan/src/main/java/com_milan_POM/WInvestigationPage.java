e3c9cac16e42eb658a994320d473d1fa2c7cb61b
paragb <paragb@IVF111.PALASH.COM>
2018-05-22 00:15:18 +0530

    investigation changes

--- IVFmilan/src/main/java/com_milan_POM/WInvestigationPage.java
+++ IVFmilan/src/main/java/com_milan_POM/WInvestigationPage.java
@@ -3,64 +3,136 @@ package com_milan_POM;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
 
 import com_Milan_Base.TestBase;
 import com_Milan_Excelutility.Exls_Reader;
+import com_Milan_util.TestUtil;
 
-public class WInvestigationPage extends TestBase
-{
-	@FindBy(xpath="//li[text()='Cycles']") WebElement Cycles;
-	@FindBy(xpath="(//input[@name='txtServiceName'])[2]")WebElement Search;
+public class WInvestigationPage extends TestBase {
+	@FindBy(xpath = "//li[text()='Cycles']")
+	WebElement Cycles;
+	@FindBy(xpath = "(//input[@name='txtServiceName'])[2]")
+	WebElement Search;
+	@FindBy(xpath = "//span[@class='icon-screen ng-binding']")
+	WebElement InvestigationPageTitle;
+	@FindBy(xpath = "//span[@class='icon-screen ng-binding']")WebElement fornegetiveflashmsg;
+	@FindBy(xpath="//div[@class='close-button ng-scope']")WebElement closeflash;
+@FindBy(xpath="//*[@class='toast-text ng-scope']//span//following::span")WebElement saveflashmessage;
 	
-	Exls_Reader reader = new Exls_Reader("C:\\Parag\\Git\\IVFmilan\\src\\main\\java\\com_Milan_TestData\\Milandata.xlsx");
-	int count=0;
-	int rows=1;
-	WInvestigationPage()
-	{
+
+	Exls_Reader reader = new Exls_Reader(
+			"C:\\Parag\\Git\\IVFmilan\\src\\main\\java\\com_Milan_TestData\\Milandata.xlsx");
+	int count = 0;
+	int rows = 1;
+	int rows1=2;
+	int count1 = 0;
+	int count2;
+
+	WInvestigationPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
-	public int Setsearchvalue()
+
+	public int Setsearchvalue() throws Exception 
 	{
-		System.out.println();
+
 		Cycles.click();
-	String Name=reader.getCellData("Investigation", "Search", 2);
+		String Name = reader.getCellData("Investigation", "Search", 2);
 		Search.sendKeys(Name);
-		List<WebElement>searchlist= driver.findElements(By.xpath("//ul[@class='dropdown-menu ng-isolate-scope']/li"));
-		int size= searchlist.size();
-		
-		for(int i=0;i<size;i++)
-		{
+		Thread.sleep(1000);
+		Search.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(1000);
+		Search.sendKeys("f");
+		List<WebElement> searchlist = driver.findElements(By.xpath("//ul[@class='dropdown-menu ng-isolate-scope']/li"));
+		int size = searchlist.size();
+
+		for (int i = 0; i <= 8; i++) {
 			rows++;
-			
-			
-			String resultnames= searchlist.get(i).getText();
-			
-			while(rows<size)
-			{
-			
-			reader.setCellData("Investigation", "Searchresult", rows, resultnames);
-			
-			break;
+			if (rows == 8) {
+				break;
+			} else {
+				String resultnames = searchlist.get(i).getText();
+
+				while (rows <= size) 
+				{
+
+					reader.setCellData("Investigation", "Searchresult", rows, resultnames);
+
+					break;
+				}
+
 			}
-			
 		}
 		return size;
 	}
-		
-		
-		
-	
-	public void ARTCycle()
+
+	public int REFIVFPACKAGEARTCycle() throws Exception 
 	{
+		Cycles.click();
+		String Name = reader.getCellData("Investigation", "Search", 2);
+		Search.sendKeys(Name);
+		Thread.sleep(1000);
+		Search.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(1000);
+		Search.sendKeys("f");
+		List<WebElement> searchlist = driver.findElements(By.xpath("//ul[@class='dropdown-menu ng-isolate-scope']/li"));
+		int size = searchlist.size();
+
+		int i = 1; rows=2;
+		/*while (i <= size) 
+		{*/
+			
+			String cyclename = reader.getCellData("Investigation", "Searchresult", 3);
+			String ARTName = searchlist.get(i).getText();
+			if (cyclename.equals(ARTName)) 
+			{
+				searchlist.get(i).click();
+				
+			}
+			Thread.sleep(2000);
+			WebElement ArtType = driver.findElement(By.xpath("(//th[text()='ART Type']//following::select)[1]"));
+			String ARTTypechar = reader.getCellData("Investigation", "REF. IVF PACKAGE", 5);
+			Select ArtType1 = new Select(ArtType);
+			List<WebElement>selectoptions=ArtType1.getOptions();
+			int size1= selectoptions.size();
+			for(int j=1;j<size1;j++)
+			{
+				String arr1;
+			String OptionNames =  selectoptions.get(j).getText();
+
+			
+			String namesoptions =  reader.getCellData("Investigation", "REF. IVF PACKAGE", rows1);
+			rows1++;
+			if(OptionNames.equals(namesoptions))
+			{
+				count++;
+				
+			}
+			count2= count;
+			
+			
+
+			}
+			
+
+		return count2;
+		}
 		
+
+
+
+	
+	
+
+	@AfterMethod
+	public void Teardown() 
+	{
+		driver.close();
 	}
-	
-	
-	
-	
 
 }
--- IVFmilan/src/test/java/com_Milan_Test/WInvestigationPageTest.java
+++ IVFmilan/src/test/java/com_Milan_Test/WInvestigationPageTest.java
@@ -1,5 +1,6 @@
 package com_Milan_Test;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,7 +14,7 @@ import com_milan_POM.SearchPage;
 import com_milan_POM.WInvestigationPage;
 import com_milan_POM.WomenHistoryPage;
 
-public class WInvestigationPageTest extends TestBase
+public class WInvestigationPageTest extends TestBase 
 {
 	HomePage HomePage;
 	Loginpage Loginpage;
@@ -22,36 +23,42 @@ public class WInvestigationPageTest extends TestBase
 	AddictionsPage Addictions;
 	SearchPage SearchPage;
 	WInvestigationPage Investigation;
-	Exls_Reader reader = new Exls_Reader("C:\\Parag\\Git\\IVFmilan\\src\\main\\java\\com_Milan_TestData\\Milandata.xlsx");
-	
-	public WInvestigationPageTest()
-	{
+	Exls_Reader reader = new Exls_Reader(
+			"C:\\Parag\\Git\\IVFmilan\\src\\main\\java\\com_Milan_TestData\\Milandata.xlsx");
+	int exp, Expected;
+
+	public WInvestigationPageTest() {
 		super();
 	}
+
 	@BeforeMethod
-	public void Setup() throws Exception
+	public void Setup() throws Exception 
 	{
 		TestBase.initalization();
-		Loginpage= new Loginpage();
+		Loginpage = new Loginpage();
 		HomePage = Loginpage.Verifylogin(prop.getProperty("username"), prop.getProperty("password"));
 		EMRPage = HomePage.searchPaient();
-		//EMRPage= HomePage.ClickonEMR();
-		//EMRPage=HomePage.SearchusingCalender();
-		Investigation= EMRPage.ClickOnInvestigation();
-		
-	}
-	@Test
-	public void CycleCreationTest()
-	{
-	int Actual =Investigation.Setsearchvalue();
-	
-	for(int rows=2;rows<=7;rows++)
-	{
-		String Expected1 = reader.getCellData("Investigation", "Searchresult", rows);
-	}
-	
-		
-	}
-	
+		// EMRPage= HomePage.ClickonEMR();
+		// EMRPage=HomePage.SearchusingCalender();
+		Investigation = EMRPage.ClickOnInvestigation();
 
+	}
+
+	@Test(priority=1,enabled=false)
+	public void CycleCreationTest() throws Exception 
+	{
+		int Actual = Investigation.Setsearchvalue();
+		int Expected = reader.getRowCount("Investigation");
+		Assert.assertEquals(Actual, Expected);
+	}
+	
+	@Test(priority=2)
+	public void REFIVFPACKAGEARTCycleTest() throws Exception
+	{
+	int Actual =Investigation.REFIVFPACKAGEARTCycle();
+	int Expected = 6;
+	Assert.assertEquals(Actual, Expected);
+	
+		
+	}
 }
