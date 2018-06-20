fec32a7adb105be4d11b74924b36d55a8470d98d
paragb <paragb@IVF111.PALASH.COM>
2018-06-20 22:49:29 +0530

    cycle overview

---
+++ IVFmilan/src/main/java/com_milan_POM/CycleOverviewPage.java
@@ -0,0 +1,160 @@
+package com_milan_POM;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com_Milan_Base.TestBase;
+import com_Milan_util.TestUtil;
+
+public class CycleOverviewPage extends TestBase 
+{
+	private @FindBy (xpath="//label[@class='checkbox-inline p-t-0']/input")WebElement Finalize;
+	private @FindBy(xpath="//li[text()='Stimulation Chart']")WebElement StimulationChart;
+	private @FindBy(xpath="//li[text()='Semen Details']")WebElement SemenDetails ;
+	private @FindBy(xpath="//li[text()='OPU']")WebElement OPU ;
+	private @FindBy(xpath="//li[text()='Embryology']")WebElement Embryology ;
+	private @FindBy(xpath="//li[text()='Embryo Transfer']")WebElement EmbryoTransfer ;
+	private @FindBy(xpath="//li[contains(text(), 'Cryo Preservation')]")WebElement CryoPreservation;
+	private @FindBy(xpath="//li[contains(text(), 'Outcome')]")WebElement Outcome;
+	private @FindBy(xpath=" //button[contains(text(), 'Add Stimulation Drug')]")WebElement AddSimulation;
+	private @FindBy(xpath="//span[@class='toast-msg ng-binding ng-scope']")WebElement UpdateMessage;
+	private @FindBy(xpath="//button[text()='New Cycle']")WebElement Newcycle;
+	private @FindBy(xpath="//label[text()='Source of Sperm']/following-sibling::div/select")WebElement SourceofSperm; 
+	private @FindBy(xpath="//label[contains (text(), 'Method of Semen Collection')]/following-sibling::div/select")WebElement MethodofSemenCollection;
+	private @FindBy(xpath="//label[text()='ART Type']/following-sibling::div/select")WebElement ARTType;
+	private @FindBy(xpath="//label[text()='ART Type']//following::select[2]")WebElement ARTSubType;
+	private @FindBy(xpath="//label[contains (text(), 'Protocol')]/following-sibling::div/select")WebElement Protocol;
+	
+	String msg;
+	
+	
+	
+	
+	CycleOverviewPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	public String ValueInSourceofSperm() 
+		{
+		msg = SourceofSperm.getAttribute("value");
+		return msg;
+		
+	}
+	
+	
+	public boolean EnableconditionAddSimulationDrug()
+	{
+		
+		Actions act = new Actions(driver);
+		act.moveToElement(StimulationChart).click().perform();
+		JavascriptExecutor js = (JavascriptExecutor) driver;  
+		js.executeScript("scroll(0, 250);");
+		boolean flag = true;
+		act.moveToElement(Finalize);
+		System.out.println("Finalize checkbox is displayed" + Finalize.isDisplayed()+"Finalize checkbox is enabled" + Finalize.isEnabled() );
+		boolean Flag1=Finalize.isSelected();
+		if(Flag1==true)
+		{
+			 flag= AddSimulation.isEnabled();
+			return flag;
+			
+		}
+		return flag;
+	
+		
+	}
+	
+	public boolean Noofcycle()
+	{
+		List<WebElement>cycles = driver.findElements(By.xpath("//table[@class='table table-hover table-striped']//tbody//tr"));
+		int size = cycles.size();
+		boolean flag= false;
+		if(size>0)
+		{
+			flag= true;
+		}
+		return flag;
+		
+	}
+	public boolean EnabledconditionNewCycle()
+	{
+		boolean flag= false;
+		Actions act = new Actions(driver);
+		act.moveToElement(Newcycle);
+		flag= Newcycle.isEnabled();
+		return flag;
+	}
+	public String SourceofSperm() throws Exception
+	{
+		Select SOS = new Select(SourceofSperm);
+		Thread.sleep(2000);
+		WebElement we = SOS.getFirstSelectedOption();
+		Actions act = new Actions(driver);
+		act.moveToElement(we);
+		Thread.sleep(4000);
+		msg= we.getText();
+		
+		
+		return msg;
+	}
+	public String ValueInMethodOfSemenCollectionOption() throws Exception
+	{
+		Select MOS = new Select(MethodofSemenCollection);
+		Thread.sleep(1000);
+		
+		WebElement we = MOS.getFirstSelectedOption();
+		Actions act = new Actions(driver);
+		act.moveToElement(we);
+		Thread.sleep(3000);
+		msg= we.getText();
+		try
+		{
+			TestUtil.VisibleOn(driver, we, 30);
+		}
+		catch(TimeoutException e)
+		{
+			System.out.println("Element" + we + "is not seen within 30 sec" );
+		}
+		
+		return msg;
+		
+	}
+	public String ValueInProtocol() throws Exception
+	{
+		Select VOP = new Select(Protocol);
+		Thread.sleep(1000);
+		WebElement we = VOP.getFirstSelectedOption();
+		Actions act = new Actions(driver);
+		act.moveToElement(we);
+		Thread.sleep(3000);
+		msg= we.getText();
+		try
+		{
+			TestUtil.VisibleOn(driver, we, 30);
+		}
+		catch(TimeoutException e)
+		{
+			System.out.println("Element" + we + "is not seen within 30 sec" );
+		}
+		String msg1=we.getText();
+		if(msg.equals(msg1))
+		{
+			return msg1;
+		}
+		return msg;
+		
+		
+		
+		
+		
+	}
+		
+	}
---
+++ IVFmilan/src/test/java/com_Milan_Test/CycleOverviewPageTest.java
@@ -0,0 +1,129 @@
+package com_Milan_Test;
+
+import static org.testng.Assert.assertFalse;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import org.junit.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com_Milan_Base.TestBase;
+import com_Milan_Excelutility.Exls_Reader;
+import com_milan_POM.CycleListPage;
+import com_milan_POM.CycleOverviewPage;
+import com_milan_POM.EMRDashBoardPage;
+import com_milan_POM.HomePage;
+import com_milan_POM.Loginpage;
+import com_milan_POM.WInvestigationPage;
+import com_milan_POM.WOPUCycyclePage;
+
+public class CycleOverviewPageTest extends TestBase 
+{
+	Loginpage Loginpage;
+	HomePage HomePage;
+	EMRDashBoardPage EMRPage;
+	WInvestigationPage Investigation;
+	WOPUCycyclePage WOC;
+	CycleListPage CLP;
+	CycleOverviewPage COP;
+	Exls_Reader reader = new Exls_Reader("C:\\Parag\\Git\\IVFmilan\\src\\main\\java\\com_Milan_TestData\\Milandata.xlsx");
+	
+	
+	
+	
+	
+	
+	
+	CycleOverviewPageTest()
+	{
+		super();
+	}
+	
+	
+	@BeforeMethod
+	public void Setup() throws Exception
+	{
+		TestBase.initalization();
+		Loginpage = new Loginpage();
+		HomePage = Loginpage.Verifylogin(prop.getProperty("username"), prop.getProperty("password"));
+		EMRPage = HomePage.searchPaient();
+		Investigation = EMRPage.ClickOnInvestigation();
+		WOC = new WOPUCycyclePage();
+		
+		boolean flag = WOC.AlreadySavedCycle();
+		if(flag==true)
+		{
+			WOC.AddExistionService();
+		}
+		else
+		{
+
+			 boolean flag1= WOC.Existingcycle();
+			 if(flag1==false)
+			 {
+				 WOC.SaveOPUsubtypeICSI();
+
+			 }
+			 else
+			 {
+				 System.out.println("cycle already exist");
+			 }
+		}		
+		CLP= WOC.ClickonCycleOption();
+		boolean flag1 = CLP.NewCycleButtonEnableCondition();
+		if(flag1==true)
+		{
+			CLP.SaveTheCycle();	
+		}
+		else
+		{
+			System.out.println("User has already saved cycle");
+		}
+		COP = CLP.ClickOnCycleCode();
+	}
+	
+	
+
+	@Test(priority=6,enabled=false)
+	public void EnableconditionAddSimulationDrugTest()
+	{
+		boolean flag= COP.EnableconditionAddSimulationDrug();
+		assertFalse(flag);
+		System.out.println("EnableconditionAddSimulationDrugTest is completed");
+	}
+	@Test(priority=1,enabled=true)
+	public void ValueInSourceofSpermOptionTest() throws Exception
+	{
+		String act= COP.SourceofSperm();
+		String exp = CLP.SourceofspermPartner();
+		Assert.assertEquals(act, exp);
+		System.out.println("ValueInSourceofSpermOptionTest is compleed");
+	}
+		
+		@Test(priority=2,enabled=true)
+		public void ValueInMethodOfSemenCollectionOptionTest() throws Exception
+		{
+			String act= COP.ValueInMethodOfSemenCollectionOption();
+			String exp = CLP.MethodofSemenCollection();
+			Assert.assertEquals(act, exp);
+			System.out.println("ValueInMethodOfSemenCollectionOptionTest is completed");
+		}
+		@Test(priority=3,enabled=true)
+		public void ValueInProtocolTest() throws Exception
+		{
+			String act= COP.ValueInProtocol();
+			String exp = CLP.NoofProtocol();
+			Assert.assertEquals(act, exp);
+			System.out.println("ValueInProtocoltest is completed");
+		}
+		
+	@AfterMethod
+	public void TearDown()
+	{
+		driver.quit();
+	}
+	
+}
