0e72061e3a6ae31d5e12be812569996b9f823470
paragb <paragb@IVF111.PALASH.COM>
2018-07-24 16:52:10 +0530

    registrationPage

---
+++ IVFmilan/src/test/java/com_Milan_Test/RegistrationPageTest.java
@@ -0,0 +1,27 @@
+package com_Milan_Test;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com_Milan_Base.TestBase;
+
+public class RegistrationPageTest extends TestBase 
+{
+	
+	RegistrationPageTest()
+	{
+		super();
+	}
+	@BeforeMethod
+	public void SetUp()
+	{
+		
+	}
+	
+	@Test
+	public void RegisteraPatient()
+	{
+		
+	}
+
+}
